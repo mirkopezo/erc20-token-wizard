7c15f71ad6ef82ebebc50eab604a37b48c929433
Mirko Pezo <fireover@tutanota.com>
2021-07-29 23:17:50 +0200

    Small change

--- contracts/migrations/2_deploy_contract.js
+++ contracts/migrations/2_deploy_contract.js
@@ -1,6 +1,5 @@
 const InternshipERC20Token = artifacts.require('InternshipERC20Token');
 
 module.exports = async function (deployer) {
-    web3.eth.defaultAccount = web3.eth.accounts[0];
     await deployer.deploy(InternshipERC20Token);
 };
