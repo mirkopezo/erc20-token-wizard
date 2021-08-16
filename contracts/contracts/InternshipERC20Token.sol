cc12c48c37f2a79a1bacb1094a510c7d337ecadd
Mirko Pezo <fireover@tutanota.com>
2021-08-16 14:10:51 +0200

    Coded smart contract that can create new ERC20 tokens

--- contracts/contracts/InternshipERC20Token.sol
+++ contracts/contracts/InternshipERC20Token.sol
@@ -3,13 +3,38 @@ pragma solidity ^0.8.5;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract InternshipERC20Token is ERC20 {
+    event newERC20Contract(address contractAddress);
+    
     constructor() ERC20('Crypto Internship Token', 'CIT') {
     }
 
     function decimals() public view virtual override returns (uint8) {
         return 8;
     }
+    
+    function mint(uint amount) external {
+        _mint(msg.sender, amount);
+    }
+	
+	function newERC20Token(string memory name, string memory symbol, uint initialSupply) public returns(address newContract) {
+		ERC20Token c = (new ERC20Token)(name, symbol, initialSupply, msg.sender);
+		emit newERC20Contract(address(c));
+		return address(c);
+	}
+}
 
+contract ERC20Token is ERC20 {
+    address public creator;
+
+    constructor(string memory name, string memory symbol, uint initialSupply, address _creator) ERC20(name, symbol) {
+        creator = _creator;
+        _mint(creator, initialSupply);
+    }
+    
+    function decimals() public view virtual override returns (uint8) {
+        return 8;
+    }
+    
     function mint(uint amount) external {
         _mint(msg.sender, amount);
     }
