cec82d21c7abf84724bff24d485cb88a89a380c4
mirkopezo <accfireover@tutanota.com>
2021-12-29 16:47:13 +0100

    Fix formatting and do some cleanup

--- contracts/contracts/InternshipERC20Token.sol
+++ contracts/contracts/InternshipERC20Token.sol
@@ -1,41 +1,57 @@
 pragma solidity ^0.8.5;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract InternshipERC20Token is ERC20 {
     address public lastContractAddress;
-    
-    constructor() ERC20('Crypto Internship Token', 'CIT') {
-    }
+
+    constructor() ERC20("Crypto Creator Token", "CCT") {}
 
     function decimals() public view virtual override returns (uint8) {
         return 8;
     }
-    
-    function mint(uint amount) external {
+
+    function mint(uint256 amount) external {
         _mint(msg.sender, amount);
     }
-	
-	function newERC20Token(string memory name, string memory symbol, uint initialSupply) external {
-		ERC20Token c = (new ERC20Token)(name, symbol, initialSupply, msg.sender);
-        lastContractAddress = address(c);
-	}
+
+    function newERC20Token(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) external {
+        ERC20Token token = new ERC20Token(
+            name,
+            symbol,
+            initialSupply,
+            msg.sender
+        );
+        lastContractAddress = address(token);
+    }
 }
 
 contract ERC20Token is ERC20 {
     address public creator;
 
-    constructor(string memory name, string memory symbol, uint initialSupply, address _creator) ERC20(name, symbol) {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address _creator
+    ) ERC20(name, symbol) {
         creator = _creator;
         _mint(creator, initialSupply);
     }
-    
+
     function decimals() public view virtual override returns (uint8) {
         return 8;
     }
-    
-    function mint(uint amount) external {
-        require(msg.sender == creator, 'Mint is allowed only to the creator of token');
+
+    function mint(uint256 amount) external {
+        require(
+            msg.sender == creator,
+            "Mint is allowed only to the creator of token"
+        );
         _mint(msg.sender, amount);
     }
 }
